3813501e7333625b081d3a34639de3112ecea2c8
India Rex <india.rex@and.digital>
2021-05-13 10:24:13 +0100

    Tidy up setuptest.js

--- src/setupTest.js
+++ src/setupTest.js
@@ -1,5 +1,4 @@
-// import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import Enzyme from 'enzyme';
-// configure({ adapter: new Adapter() });
+
 Enzyme.configure({ adapter: new Adapter() });
