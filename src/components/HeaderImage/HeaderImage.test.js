1fda9fd0177cd0b432f54e8b3113685db0ac80d1
India Rex <india.rex@and.digital>
2021-05-18 15:06:33 +0100

    Ready for tests to be written once components changed to functions

--- src/components/HeaderImage/HeaderImage.test.js
+++ src/components/HeaderImage/HeaderImage.test.js
@@ -1,13 +1,43 @@
-import React from 'react';
-import { mount, shallow } from 'enzyme';
-import HeaderImage from '../components/HeaderImage';
+import React from "react";
+import HeaderImage from "../HeaderImage";
+import { render, fireEvent, screen } from "@testing-library/react";
+
+// test the progress is accurate when you add a new item
+// test the progress is accurate with the initial props passed in
 
 it("knows how much progress has been made", () => {
-    const items = [
-        { id: 1, title: "thing1", location: "there", dueDate: "31-05-2021", dueTime: "1400", done: true }, 
-        { id: 1, title: "thing2", location: "here", dueDate: "31-05-2021", dueTime: "1300", done: true },
-        { id: 1, title: "thing3", location: "there", dueDate: "31-05-2021", dueTime: "0900", done: false },
-        { id: 1, title: "thing4", location: "here", dueDate: "31-05-2021", dueTime: "1000", done: false }
-    ];
-
-})
+  const items = [
+    {
+      id: 1,
+      title: "thing1",
+      location: "there",
+      dueDate: "31-05-2021",
+      dueTime: "1400",
+      done: true,
+    },
+    {
+      id: 1,
+      title: "thing2",
+      location: "here",
+      dueDate: "31-05-2021",
+      dueTime: "1300",
+      done: true,
+    },
+    {
+      id: 1,
+      title: "thing3",
+      location: "there",
+      dueDate: "31-05-2021",
+      dueTime: "0900",
+      done: false,
+    },
+    {
+      id: 1,
+      title: "thing4",
+      location: "here",
+      dueDate: "31-05-2021",
+      dueTime: "1000",
+      done: false,
+    },
+  ];
+});
