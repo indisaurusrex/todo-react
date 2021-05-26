ae23cba8c0225e72a6fe73c110c2119a188b70da
India Rex <india.rex@and.digital>
2021-05-26 11:17:01 +0100

    Test for header progress from dummy data is working, test for adding a new todo and recalculating progress is not working

--- src/components/HeaderImage/HeaderImage.test.js
+++ src/components/HeaderImage/HeaderImage.test.js
@@ -1,49 +1,56 @@
 import React from "react";
-import { HeaderImage, Progress } from "../index";
-import { render, fireEvent, screen } from "@testing-library/react";
+import { HeaderImage } from "../index";
+import { render, fireEvent, screen, getByText, waitFor } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import 'regenerator-runtime/runtime';
 
 // test the progress is accurate when you add a new item
 // test the progress is accurate with the initial props passed in
+const basicItems = [
+  {
+    id: 1,
+    title: "thing1",
+    location: "there",
+    dueDate: "31-05-2021",
+    dueTime: "1400",
+    done: true,
+  },
+  {
+    id: 2,
+    title: "thing2",
+    location: "here",
+    dueDate: "31-05-2021",
+    dueTime: "1300",
+    done: true,
+  },
+  {
+    id: 3,
+    title: "thing3",
+    location: "there",
+    dueDate: "31-05-2021",
+    dueTime: "0900",
+    done: false,
+  },
+  {
+    id: 4,
+    title: "thing4",
+    location: "here",
+    dueDate: "31-05-2021",
+    dueTime: "1000",
+    done: false,
+  },
+];
 
 it("knows how much progress has been made", () => {
-  const items = [
-    {
-      id: 1,
-      title: "thing1",
-      location: "there",
-      dueDate: "31-05-2021",
-      dueTime: "1400",
-      done: true,
-    },
-    {
-      id: 2,
-      title: "thing2",
-      location: "here",
-      dueDate: "31-05-2021",
-      dueTime: "1300",
-      done: true,
-    },
-    {
-      id: 3,
-      title: "thing3",
-      location: "there",
-      dueDate: "31-05-2021",
-      dueTime: "0900",
-      done: false,
-    },
-    {
-      id: 4,
-      title: "thing4",
-      location: "here",
-      dueDate: "31-05-2021",
-      dueTime: "1000",
-      done: false,
-    },
-  ];
-
-  render(<HeaderImage items={items} />)
-  const percentage = screen.findByText(50);
-  // expect(percentage).toBeTruthy;
-  // what is going here? how do you check that there's 50% now and then add one
-  // and check it's changed to 40%
+  render(<HeaderImage items={basicItems} />)
+  expect(screen.getByRole('header-progress')).toHaveTextContent(50 + '% done')
 });
+
+// this test isn't working, how do you get the async and buttons to work?
+
+// it("recalculates the progress when a new todo is added", async () => {
+//   render(<HeaderImage items={basicItems} />)
+//   fireEvent.click(screen.getByText('Add an item'))
+//   await waitFor(() => fireEvent.click(screen.getByText('Go!')))
+//   expect(screen.getByRole('header-progress')).toHaveTextContent(50 + '%')
+// });
--- src/components/Progress/Progress.js
+++ src/components/Progress/Progress.js
@@ -3,7 +3,7 @@ import React from "react";
 function Progress(props) {
   return (
     <div className="progress-in-header">
-      <p>{props.donePercent}% done</p>
+      <p role="header-progress">{props.donePercent}% done</p>
     </div>
   );
 }
