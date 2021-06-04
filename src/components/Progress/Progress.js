864972058d26a17238b0fc09234b60bde2d25b45
India Rex <india.rex@and.digital>
2021-06-04 15:32:09 +0100

    Progress circle added and working

--- src/components/Progress/Progress.js
+++ src/components/Progress/Progress.js
@@ -1,8 +1,10 @@
 import React from "react";
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 function Progress(props) {
   return (
     <div className="progress-in-header">
+      <CircularProgress variant="determinate" value={props.donePercent} />
       <p role="header-progress">{props.donePercent}% done</p>
     </div>
   );
