3e7c291f0bb2048f4a2a8422d4a18aa123865360
India Rex <india.rex@and.digital>
2021-06-16 13:26:11 +0100

    progress in header CSS module replaces progress-in-header class in app.css

--- src/components/Progress/Progress.js
+++ src/components/Progress/Progress.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import styles from './Progress.module.css';
 
 export function Progress({ donePercent }) {
   return (
-    <div className="progress-in-header">
+    <div className={styles.progressInHeader}>
       <CircularProgress
         variant="determinate"
         value={donePercent}
