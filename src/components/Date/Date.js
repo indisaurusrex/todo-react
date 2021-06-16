543f1116bb50bfd59ed1a8d9fd0a501eedf05077
India Rex <india.rex@and.digital>
2021-06-16 13:16:06 +0100

    Date CSS Module replaces date-in-header class in app.css

--- src/components/Date/Date.js
+++ src/components/Date/Date.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Moment from 'react-moment';
+import styles from './Date.module.css';
 
 export function TodoDate() {
   return (
-    <div className="date-in-header">
+    <div className={styles.dateInHeader}>
       <Moment date={new Date()} format="D MMMM YYYY" />
     </div>
   );
 }
-
-// export default TodoDate;
