5fe8d879713a30d923a09c429074db648b127525
Ashraf Nazar <ashraf.caspian@gmail.com>
2021-07-01 10:37:14 +0100

    Remove `fireEvent` event action from HeaderImage

--- src/components/HeaderImage/HeaderImage.test.js
+++ src/components/HeaderImage/HeaderImage.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import HeaderImage from './HeaderImage';
 import '@testing-library/jest-dom/extend-expect';
 import 'regenerator-runtime/runtime';
