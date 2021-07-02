8b8848a78726fd67f260d3eee5d6fc05c1b9d8ad
India Rex <india.rex@and.digital>
2021-07-02 16:43:01 +0100

    Add summary comment for Progress component

--- src/components/Progress/Progress.js
+++ src/components/Progress/Progress.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import styles from './Progress.module.css';
 
+/**
+ * Show the progress on the top of the image in the form of done percent, and the circle of done
+ */
 export default function Progress({ donePercent }) {
   return (
     <div className={styles.progressInHeader}>
