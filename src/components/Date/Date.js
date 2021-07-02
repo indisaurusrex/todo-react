5eda2d55dae85cb451515045691f3b5f7ee2dd1e
India Rex <india.rex@and.digital>
2021-07-02 16:30:18 +0100

    Add summary comment for Date component

--- src/components/Date/Date.js
+++ src/components/Date/Date.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Moment from 'react-moment';
 import styles from './Date.module.css';
 
+/** 
+ * Displays today's date on the header image
+ */
 export default function TodoDate() {
   return (
     <div className={styles.dateInHeader}>
